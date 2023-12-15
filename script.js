72d0733a36cc4cacba9602168c74744aed3fa97d
Emileeeee <inurilya04@gmail.com>
2023-12-15 20:56:42 +0600

    Create JS

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const targetDiv = document.querySelector('.target')
-
+const list = document('.list')
 const newP = document.createElement('p')
 newP.textContent = "Этот текст был создан в JS"
 targetDiv.append(newP)
+
+for (let i=0; i<5;i+=1) {
+    const nemLi = document.createElement('li')
+    newLi.textContent = '№'+ (i+1)
+    list.append(newLi)
+}
